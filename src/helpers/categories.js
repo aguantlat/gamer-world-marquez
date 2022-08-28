a3ee5d81439ed0e52fdfdbc8ecca756ff51344aa
maxidmarquez <maximiliano.marquez@indreply.de>
2022-08-28 16:41:27 -0300

    [ENTREGA-5] Catalogo de productos

---
+++ src/helpers/categories.js
@@ -0,0 +1,8 @@
+import Category from "../models/category"
+
+export const consolas = new Category(1, 'Consolas');
+
+export const videojuegos = new Category(2, 'Videojuegos');
+
+export const accesorios = new Category(3, 'Accesorios');
+
