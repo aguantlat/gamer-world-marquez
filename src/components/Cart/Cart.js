0a1a93544ed8b0d25e5fbfccd4575985a47401dc
maxidmarquez <maximiliano.marquez@indreply.de>
2022-09-19 16:56:00 -0300

    [ENTREGA-10] CART VIEW

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,24 @@
 import { useContext } from "react";
 import { Button, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
+import './Cart.scss'
 import { CartContext } from "../../context/CartContext";
 
 const Cart = () => {
-  const { cart, removeItem } = useContext(CartContext);
+  const { cart, removeItem, getTotal } = useContext(CartContext);
 
   if (cart.length === 0) {
-    return <h5 className="p-3">El carrito está vacío</h5>;
+    return (
+      <div className="p-3">
+        <h5>El carrito está vacío</h5>
+        <Link to={"/"}>
+          <Button className="mt-3" variant="primary">
+            Volver a la tienda
+          </Button>
+        </Link>
+      </div>
+    );
   }
 
   return (
@@ -35,13 +46,23 @@ const Cart = () => {
                 <strong>$ {price * quantity}</strong>
               </td>
               <td>
-                <Button variant="link" onClick={() => removeItem(id)}>
+                <Button  variant="link" onClick={() => removeItem(id)}>
                   Quitar del carrito
                 </Button>
               </td>
             </tr>
           ))}
         </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="5">
+              <h4 className="text-end">Total</h4>
+            </td>
+            <td>
+              <h4 className="text-start">${getTotal()}</h4>
+            </td>
+          </tr>
+        </tfoot>
       </Table>
     </div>
   );
